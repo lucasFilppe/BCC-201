115390700b82e27f3bb396b0add6511cf6a9d8b6
lucasFilppe <lucasfilipe2323@icloud.com>
2023-03-23 20:36:44 -0300

    funcoes

--- 2-FUNCOES/1.c
+++ 2-FUNCOES/1.c
@@ -1,17 +1,66 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 #include <stdio.h>
 
+//celsius
+double calculaCelsiusKelvin(double celsius){
+    return celsius + 273.15;
+}
+double calculaCelsiusFarenheit(double celsius){
+    return (celsius * (9/5)) + 32;     
+}
 
-//funço de conversao de graus para radianos
-double converteGrausRadianos(){
+//fução do menu para conversao de Celsius para Fareheit e Kelvin
+void menuConversaoCelsis(){
+    double celsius;
+    
+    printf("Digite o valor em Celsius: ");
+    scanf("%lf", &celsius);
+    
+    printf("Valor em Farenheit: %.2lf\n", calculaCelsiusFarenheit(celsius));
+    printf("Valor em Kelvin: %.2lf\n", calculaCelsiusKelvin(celsius));
+    
+}
+
+//função que calcula graus para radianos
+double calculaGrausRadianos(double graus){
     double pi = 3.14;
+    return (graus * (pi / 180));
+}
 
-    double graus, radianos;
+//funçao de menu conversao de graus para radianos
+void menuConverteGrausRadianos(){
+
+    double graus;
 
     printf("Digite o valor em Graus: ");
     scanf("%lf", &graus);
 
-    double radianos = (graus * (pi / 180))
+    printf("Valor em radianos: %.2lf\n", calculaGrausRadianos(graus));
 }
+
+//função que calcula conversao de radianos em graus
+
+double calculaRadianosGraus(double radianos){
+    double pi = 3.14;
+    return (radianos * (180 / pi));
+}
+
+//fução de conversão de radianos em graus
+void menuConversaoRadianosGraus(){
+    double radianos;
+
+    printf("Digite o valor em radianos: ");
+    scanf("%lf", &radianos);
+
+    printf("Valor em graus: %.2lf\n", calculaRadianosGraus(radianos));
+}
+
 
 //função para exibir menu de conversor de angulos
 void converteAngulos(){
@@ -22,21 +71,54 @@ void converteAngulos(){
 
     printf("1) Graus\n");
     printf("2) Radianos\n");
-
+    printf("\n");
+    
     printf("Selecione uma opção: ");
     scanf("%d", &opcao);
-
+    printf("\n");
+    
     if (opcao == 1)
     {
-        converteAngulos
+        menuConverteGrausRadianos();
     }
     
+    else if(opcao == 2){
+        menuConversaoRadianosGraus();  
+    }
 
+    else{
+        printf("Opção invalida\n");
+    }
+
+}
+
+void converteTemperatura(){
+    int opcao;
+    printf("\n");
+    printf("Qual a unidade de origem:\n");
+    printf("\n");
+
+    printf("1) Celsius\n");
+    printf("2) Fareheit\n");
+    printf("3) Kelvin\n");
+    printf("\n");
+    
+    printf("Selecione uma opção: ");
+    scanf("%d", &opcao);
+    printf("\n");
+    
+    if(opcao == 1){
+        menuConversaoCelsis();
+    }
+    else{
+        printf("Entrada invalida\n");
+    }
 }
 
 //função para exibir menu conversor de unidades
 void converteUnidades(){
     int opcao;
+    printf("### COVERSOR DE UNIDADES ###\n\n");
     printf("1) Angulo\n");
     printf("2) Temperatura\n");
     printf("\n");
@@ -48,10 +130,10 @@ void converteUnidades(){
         converteAngulos();
     }
 
-    /*else if (opcao == 2)
+    else if (opcao == 2)
     {
         converteTemperatura();
-    }*/
+    }
 
     else
     {
@@ -60,11 +142,10 @@ void converteUnidades(){
     
     printf("\n");
 }
+
+
 int main(){
-
     converteUnidades();
-
-
 
     return 0;
 }
