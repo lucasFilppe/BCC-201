743534e6f28ecdcaba63ed3f2bb43d5750b5e348
lucasFilppe <lucasFilipe2323 @icloud.com>
2023-05-11 17:57:43 -0300

    repetição

---
+++ 4-lacoRepeticao/3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+//função que retora 1 se valores sao validos e 0 se nao
+int ehValido(int x , int y){
+   
+    if((x < y)  && (x % 2 == 0 && y % 2 == 0 )){
+       return 1;
+    }
+    return 0;
+}
+
+
+//função que imprime asteristico de retangulo
+void imprimeAsteristicos(int x, int y){
+    for(int i = 1; i <= x; i++) {
+        printf(" ");
+        for(int j = 1; j <= y; j++){
+            printf("*");
+        }
+       
+    }
+}
+
+
+int main(){
+
+
+    int x, y;
+
+
+    do {
+    printf("Digite os valores de x e y: ");
+    scanf("%d %d", &x, &y);
+
+
+   
+    if(x > y){
+            printf("x deve ser menor que y\n");
+        }
+
+
+    else if(x % 2 == 1 || y % 2 == 1 ){
+            printf("Apenas numeros pares sao aceitos\n");
+    }
+
+
+    else{
+        imprimeAsteristicos(x, y);
+        break;
+    }
+
+
+    }while(ehValido(x, y) != 1);
+
+
+   
+    return 0;
+}
